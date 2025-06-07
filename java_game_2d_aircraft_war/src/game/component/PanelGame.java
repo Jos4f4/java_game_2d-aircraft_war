c28b1ce06024a4afcfc26e0517a0f6ef84a52c0d
Josaf4 <josafa.soares2909@gmail.com>
2025-06-07 17:51:20 -0400

    Commit bullets

--- java_game_2d_aircraft_war/src/game/component/PanelGame.java
+++ java_game_2d_aircraft_war/src/game/component/PanelGame.java
@@ -4,9 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JComponent;
+
+import game.obj.Bullet;
+import game.obj.Player;
 
 public class PanelGame extends JComponent {
 	
@@ -16,10 +23,15 @@ public class PanelGame extends JComponent {
 	private int height;
 	private Thread thread;
 	private boolean start = true;
+	private Key key;
+	private int shotTime;
 	
 	// Game FPF
 	private final int FPS = 60;
 	private final int TARGET_TIME = 1000000000 / FPS;
+	// Game object
+	private Player player;
+	private List<Bullet> bullets;
 	
 	public void start() {
 		width = getWidth();
@@ -43,7 +55,113 @@ public class PanelGame extends JComponent {
 				}
 			}
 		});
+		initObjectgame();
+		initKeyboard();
+		initBullets();
 		thread.start();
+	}
+	
+	private void initObjectgame() {
+		player = new Player();
+		player.changeLocation(150, 150);
+	}
+	
+	private void initKeyboard() {
+		key = new Key();
+		requestFocus();
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_A) {
+					key.setKey_left(true);
+				} else if(e.getKeyCode() == KeyEvent.VK_D) {
+					key.setKey_right(true);
+				} else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+					key.setKey_space(true);
+				} else if(e.getKeyCode() == KeyEvent.VK_J) {
+					key.setKey_j(true);
+				} else if(e.getKeyCode() == KeyEvent.VK_K) {
+					key.setKey_k(true);
+				}
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_A) {
+					key.setKey_left(false);
+				} else if(e.getKeyCode() == KeyEvent.VK_D) {
+					key.setKey_right(false);
+				} else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+					key.setKey_space(false);
+				} else if(e.getKeyCode() == KeyEvent.VK_J) {
+					key.setKey_j(false);
+				} else if(e.getKeyCode() == KeyEvent.VK_K) {
+					key.setKey_k(false);
+				}
+			}
+		});
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				float s = 0.5f;
+				while(start) {
+					float angle = player.getAngle();
+					if(key.isKey_left()) {
+						angle -= s;
+					}
+					if(key.isKey_right()) {
+						angle += s;
+					}
+					if(key.isKey_j() || key.isKey_k()) {
+						if(shotTime == 0) {
+							if(key.isKey_j()) {
+								bullets.add(0, new Bullet(player.getX(), player.getY(), player.getAngle(), 5, 3f));
+							} else {
+								bullets.add(0, new Bullet(player.getX(), player.getY(), player.getAngle(), 20, 3f));
+							}
+						}
+						shotTime++;
+						if(shotTime == 15) {
+							shotTime = 0;
+						}
+					} else {
+						shotTime = 0;
+					}
+					if(key.isKey_space()) {
+						player.speedUp();
+					} else {
+						player.speedDown();
+					}
+					player.update();
+					player.changeAngle(angle);
+					sleep(5);
+				}
+			}
+		}).start();
+	}
+	
+	private void initBullets() {
+		bullets = new ArrayList<>();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (start) {
+					for(int i=0; i<bullets.size(); i++) {
+						Bullet bullet = bullets.get(i);
+						if(bullet != null) {
+							bullet.update();
+							if(!bullet.check(width, height)) {
+								bullets.remove(bullet);
+							}
+						} else {
+							bullets.remove(bullet);
+						}
+					}
+					sleep(1);
+				}
+			}
+		}).start();
 	}
 	
 	private void drawBackground() {
@@ -52,7 +170,13 @@ public class PanelGame extends JComponent {
 	}
 	
 	private void drawGame() {
-		
+		player.draw(g2);
+		for(int i=0; i<bullets.size(); i++) {
+			Bullet bullet = bullets.get(i);
+			if(bullet != null) {
+				bullet.draw(g2);
+			}
+		}
 	}
 	
 	private void render() {
